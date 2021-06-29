9583ee2391b8b584d0dd82f3c46ce79476f2969c
afanmr07 <afanmaulidrizqi@student.telkomuniversity.ac.id>
2021-06-29 22:38:32 +0700

    add sign-in

---
+++ e-Parking/TubesKPL/UserTableDriven.cs
@@ -0,0 +1,13 @@
+﻿namespace TubesKPL
+{
+    class UserTableDriven
+    {
+        public enum Username { users, althoof, iwiniwin, afanafan, michael};
+        public static string[] password = { "users12345", "123456789", "iwiniwin1", "afanafan1", "michael12"};
+
+        public static string getPassword(Username username)
+        {
+            return password[(int)username];
+        }
+    }
+}
