d3ca966bed6bbf62293bd0da5abf219c3b2318db
althoof <muhammadalthoof@gmail.com>
2021-07-02 17:51:53 +0700

    Update UnitTest

---
+++ e-Parking/TubesKPLTests/PaymentTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TubesKPL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubesKPL.Tests
+{
+    [TestClass()]
+    public class PaymentTests
+    {
+        [TestMethod()]
+        public void PaymentTest()
+        {
+            var test = new UserTableDriven();
+        }
+
+        internal class getPayment
+        {
+            private object althoof;
+
+            public getPayment(object althoof)
+            {
+                this.althoof = althoof;
+            }
+        }
+    }
+}
